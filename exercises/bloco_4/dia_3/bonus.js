11976deb7503db2b136d7cdb1f8af7fdef7327d1
Lucas André <lucas.cancado89@gmail.com>
2021-02-26 19:09:25 -0300

    Exercicio Bonus

--- exercises/bloco_4/dia_3/bonus.js
+++ exercises/bloco_4/dia_3/bonus.js
@@ -3,10 +3,12 @@ let linha = '';
 
 for (let i = ((n - 1) / 2); i >= 0; i--){
 	for (let index = 1; index <= n; index++) {
-		if (index < (n - (n - (i + 1))) || index > (n - i)) {
-			linha = linha + ' '
-		} else {
+		if (i === 0) {
 			linha = linha + '*'
+		} else if (index === (n - (n - (i + 1))) || index === (n - i)) {
+			linha = linha + '*'
+		} else {
+			linha = linha + ' '
 		}
 	}
 	console.log(linha);
---
+++ exercises/bloco_4/dia_3/gambito.js
@@ -0,0 +1,46 @@
+let queenRowPosition = 4;
+let queenColumnPosition = 4;
+
+let opponentRowPosition = 4;
+let opponentColumnPosition = 5;
+
+let diferencaRow = queenRowPosition - opponentRowPosition;
+let diferencaColumn = queenColumnPosition - opponentColumnPosition
+
+
+let succesfulAttack = false
+
+if (diferencaColumn === diferencaRow || diferencaColumn === (-1 * diferencaRow)) {
+     succesfulAttack = true
+}
+
+if (succesfulAttack === true && diferencaRow > 0 && diferencaColumn > 0) {
+    console.log('Ataque bem sucedido na diagonal inferior esquerda.')
+}
+
+if (succesfulAttack === true && diferencaRow < 0 && diferencaColumn > 0) {
+    console.log('Ataque bem sucedido na diagonal superior esquerda.')
+}
+
+if (succesfulAttack === true && diferencaRow < 0 && diferencaColumn < 0) {
+    console.log('Ataque bem sucedido na diagonal superior direita.')
+}
+
+if (succesfulAttack === true && diferencaRow > 0 && diferencaColumn < 0) {
+    console.log('Ataque bem sucedido na diagonal inferior direita.')
+}
+
+if (queenColumnPosition === opponentColumnPosition) {
+    succesfulAttack = true
+    console.log('Ataque bem sucedido na mesma coluna')
+}
+
+if (queenRowPosition === opponentRowPosition) {
+    succesfulAttack = true
+    console.log('Ataque bem sucedido na mesma linha')
+}
+
+
+if (succesfulAttack === false) {
+    console.log('Não foi dessa vez!')
+}
