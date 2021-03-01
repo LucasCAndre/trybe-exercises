590d9dd78a958b94f021f24ec70fb1b13cdb3028
Lucas André <lucas.cancado89@gmail.com>
2021-03-01 18:48:25 -0300

    Exercicios P2 ate o 6

--- exercises/bloco_4/dia_4/P2Ex5.js
+++ exercises/bloco_4/dia_4/P2Ex5.js
@@ -1,5 +1,29 @@
-let numeros = [2, 3, 2, 5, 8, 2, 3]
-
+function mostRepeatingNumber(numeros) {
+let repetitions = []
+let numeroCampeao = 0
 for (let index = 0; index < numeros.length; index++) {
-    
+    numberRepeat = 0
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[index] === numeros[i]) {
+            numberRepeat = numberRepeat + 1
+        }
+    }
+    repetitions.push(numberRepeat)
 }
+
+    let numerosMaiores = 0
+    for (let indice = 0; indice < repetitions.length; indice++) {
+        for (let i = 0; i < repetitions.length; i++) {
+            if (repetitions[indice] < repetitions[i]) {
+                numerosMaiores = numerosMaiores + 1
+            }           
+        }
+        if (numerosMaiores === 0) {
+            numeroCampeao = numeros[indice]
+        }
+        numerosMaiores = 0   
+    }
+    return numeroCampeao
+}    
+
+console.log(mostRepeatingNumber([2, 3, 2, 5, 8, 2, 3]))
