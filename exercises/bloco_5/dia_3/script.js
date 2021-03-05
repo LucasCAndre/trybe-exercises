6422cd3002e550af4bf114c00c7ee4dc12fe558d
Lucas André <lucas.cancado89@gmail.com>
2021-03-05 16:30:32 -0300

    Exercicio 1

--- exercises/bloco_5/dia_3/script.js
+++ exercises/bloco_5/dia_3/script.js
@@ -14,3 +14,22 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let liTag = document.createElement('li');
+    liTag.innerText = dezDaysList[index];
+    document.querySelector('#days').appendChild(liTag);
+    liTag.className = 'day'
+    if (liTag.innerText === '24' || liTag.innerText === '31') {
+      liTag.className = 'day holiday'
+    }
+    if (liTag.innerText === '4' || liTag.innerText === '11' || liTag.innerText === '18') {
+      liTag.className = 'day friday'
+    }
+    if (liTag.innerText === '25') {
+      liTag.className = 'day holiday friday'
+    }
+  }
+
