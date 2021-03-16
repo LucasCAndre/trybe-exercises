223f5da4d138f60691888b10254cd70784aeeb9c
Lucas André <lucas.cancado89@gmail.com>
2021-03-15 21:35:28 -0300

    Exercicios do dia done

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -8,3 +8,28 @@ for (let index = 0; index < arrEstados.length; index += 1) {
   tagOption.innerText = arrEstados[index];
   document.getElementById('estados').appendChild(tagOption);
 }
+
+function generateCurriculum() {
+  const formulario = document.querySelectorAll('form')[0];
+  for (let index = 0; index < formulario.length; index += 1) {
+    const tagp = document.createElement('p');
+    tagp.innerText = formulario[index].name;
+    if (tagp.innerText != 'undefined'){
+      document.getElementById('curriculum').appendChild(tagp);
+    }
+    const tagp1 = document.createElement('p');
+    tagp1.innerText = formulario[index].value;
+    if (tagp1.innerText != 'undefined') {
+      document.getElementById('curriculum').appendChild(tagp1);
+    }
+  }
+
+}
+
+document.getElementById('gerar-curriculo').addEventListener('click', generateCurriculum);
+
+function preventDef(event) {
+  event.preventDefault()
+}
+
+document.getElementById('formulario').addEventListener('click', preventDef);
