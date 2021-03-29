ae069a75535f8ad75aa071b0ff646b320eb5cb73
Lucas André <lucas.cancado89@gmail.com>
2021-03-29 13:42:13 -0300

    Pratica

---
+++ exercises/Bloco_8/dia_1/pratica.js
@@ -0,0 +1,9 @@
+const acordando = () => console.log('Acordando!!');
+
+const boraCafe = () => console.log('Bora tomar café!!');
+
+const dormir = () => console.log('Partiu dormir!!');
+
+const doingThings = (par1, par2) => par1() + par2();
+
+doingThings(acordando, boraCafe);
