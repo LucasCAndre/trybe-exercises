dbc8dd735231a9b20da9b80248b8e762ea373d87
Lucas André <lucas.cancado89@gmail.com>
2021-03-30 15:45:45 -0300

    ex5

--- exercises/Bloco_8/dia_2/ex5.js
+++ exercises/Bloco_8/dia_2/ex5.js
@@ -63,20 +63,10 @@ const books = [
   },
 ];
 
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
+const expectedResult = false;
 
-function getNamedBook() {
-  // escreva seu código aqui
-  return books.find((book) => book.name.length === 26)
+function everyoneWasBornOnSecXX() {
+  return books.every((book) => book.author.birthYear > 1900 && book.author.birthYear <= 2000);
 }
 
-assert.deepStrictEqual(getNamedBook(), expectedResult);
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
