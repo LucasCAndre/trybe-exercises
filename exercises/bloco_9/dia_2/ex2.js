38c07ff63743c3ee67f3297036d0924872f0f4de
Lucas André <lucas.cancado89@gmail.com>
2021-04-13 18:22:08 -0300

    ex2, 3, 4

---
+++ exercises/bloco_9/dia_2/ex2.js
@@ -0,0 +1,26 @@
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const arr = [];
+    const num1 = Math.ceil(Math.random() * 50);
+    const num2 = Math.ceil(Math.random() * 50);
+    const num3 = Math.ceil(Math.random() * 50);
+    const num4 = Math.ceil(Math.random() * 50);
+    const num5 = Math.ceil(Math.random() * 50);
+    const num6 = Math.ceil(Math.random() * 50);
+    const num7 = Math.ceil(Math.random() * 50);
+    const num8 = Math.ceil(Math.random() * 50);
+    const num9 = Math.ceil(Math.random() * 50);
+    const num10 = Math.ceil(Math.random() * 50);
+    arr.push(num1, num2, num3, num4, num5, num6, num7, num8, num9, num10);
+    const arrSquared = arr.map((num) => num * num);
+    const arrSum = arrSquared.reduce((acc, cur) => acc + cur);
+    if (arrSum < 8000) {
+      resolve(arrSum)
+    } else {
+      reject(arrSum)
+    }
+  }).then((respon) => console.log(`Promise resolvida: ${[respon / 2, respon / 3, respon / 5, respon / 10]}`))
+  .catch((error) => console.log(`É mais de oito mil! Essa promise deve estar quebrada!`));
+};
+
+fetchPromise()
