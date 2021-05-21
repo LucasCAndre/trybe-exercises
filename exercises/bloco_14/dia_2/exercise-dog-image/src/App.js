7611213d2b64490435496d35a0794ed209768e4a
Lucas André <lucas.cancado89@gmail.com>
2021-05-21 14:19:14 -0300

    Pratica fim

--- exercises/bloco_14/dia_2/exercise-dog-image/src/App.js
+++ exercises/bloco_14/dia_2/exercise-dog-image/src/App.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import ValidEmail from './ValidEmail';
+import React from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
+      nome: '',
       email: '',
-      saveEmail: '',
     };
   }
 
-  changeEmail(value) {
-    this.setState({ email: value });
-  }
-
-  changeSaveEmail(value) {
-    this.setState({ saveEmail: value, email: '' });
+  handleInput(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    this.setState({ [name]: value });
   }
 
   render() {
-    const { email, saveEmail } = this.state;
     return (
-      <div className="App">
-        <label htmlFor="id-email">
-          Email
-        </label>
-        <input
-          id="id-email"
-          value={ email }
-          type="email"
-          onChange={ (e) => this.changeEmail(e.target.value) }
-        />
-        <input
-          id="btn-enviar"
-          type="button"
-          data-testid="id-send"
-          value="Enviar"
-          onClick={ () => this.changeSaveEmail(email) }
-        />
-        <input id="btn-id" type="button" value="Voltar" />
-        <ValidEmail email={ saveEmail } />
+      <div>
+        <h1>Teste de inputs</h1>
+        <p>
+          Nome:
+          <input
+            onChange={(e) => this.handleInput(e)}
+            name='nome'
+            data-testid='input-nome'
+            value={this.state.nome}
+          />
+        </p>
+        <p>
+          E-mail:
+          <input
+            onChange={(e) => this.handleInput(e)}
+            name='email'
+            data-testid='input-email'
+            value={this.state.email}
+          />
+        </p>
       </div>
     );
   }
--- exercises/bloco_14/dia_2/exercise-dog-image/src/App.test.js
+++ exercises/bloco_14/dia_2/exercise-dog-image/src/App.test.js
@@ -1,44 +1,16 @@
+// App.test.js
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react'
 import App from './App';
-
-test('Verificando se existe o campo Email.', () => {
-  const { getByLabelText } = render(<App />);
-  const inputEmail = getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail.type).toBe('email');
-});
-
-test('Verificando se existe um botão de enviar', () => {
+it('alterando o valor dos campos e testando o valor guardado', () => {
   const { getByTestId } = render(<App />);
-  const btn = getByTestId('id-send');
-  expect(btn).toBeInTheDocument();
-  expect(btn.type).toBe('button');
-  expect(btn).toHaveValue('Enviar');
-});
+  const inputNome = getByTestId('input-nome');
+  expect(inputNome).toHaveValue('');
+  fireEvent.change(inputNome, { target: { value: 'Estudante da Trybe' } });
+  expect(inputNome).toHaveValue('Estudante da Trybe');
 
-test('Verificando se existe dois botões', () => {
-  const { getAllByRole } = render(<App />);
-  const buttons = getAllByRole('button');
-  expect(buttons.length).toBe(2);
-});
-
-// modifique o import abaixo
-
-// Adicione esse teste.
-test('Verificando se o botão e o campo email estão funcionando.', () => {
-  const { getByTestId, getByLabelText } = render(<App />);
-
-  const EMAIL_USER = 'email@email.com';
-
-  const textEmail = getByTestId('id-email-user');
-  expect(textEmail).toBeInTheDocument();
-  expect(textEmail).toHaveTextContent('Valor:');
-
-  const btnSend = getByTestId('id-send');
-  const inputEmail = getByLabelText('Email');
-  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
-  fireEvent.click(btnSend);
+  const inputEmail = getByTestId('input-email');
   expect(inputEmail).toHaveValue('');
-  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+  fireEvent.change(inputEmail, { target: { value: 'estudante@trybe.com' } });
+  expect(inputEmail).toHaveValue('estudante@trybe.com');
 });
