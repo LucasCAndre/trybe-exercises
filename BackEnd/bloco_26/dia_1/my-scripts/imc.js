f8b8f1fd78d58bdfe6aa721677f7ec793c80e5b2
Lucas André <lucas.cancado89@gmail.com>
2021-08-25 10:18:15 -0300

    m

--- BackEnd/bloco_26/dia_1/my-scripts/imc.js
+++ BackEnd/bloco_26/dia_1/my-scripts/imc.js
@@ -16,7 +16,7 @@ console.log(`Seu IMC é de ${theImc}`);
 const imcSituation = (imc) => {
   if (imc < 18.5) return "Abaixo do peso (magreza)";
   if (imc >= 18.5 && imc <= 24.9) return "Peso normal";
-  if (imc >= 25 && imc <= 29.9) return "Acima do peso (sobrepeso)";
+  if (imc >= 25 && imc <= 29.9) return "Ta magrimmm e fofim";
   if (imc >= 30 && imc <= 34.9) return "Obesidade grau I";
   if (imc >= 35 && imc <= 39.9) return "Obesidade grau II";
   if (imc >= 40) return "Obesidade grau III";
--- BackEnd/bloco_26/dia_1/my-scripts/sorteio.js
+++ BackEnd/bloco_26/dia_1/my-scripts/sorteio.js
@@ -5,7 +5,7 @@ const anscheck = () => {
   const ranNum = Math.round(Math.random() * 10);
   const guessedNum = readline.questionInt("Chuta um número de 1 a 10: ");
   if (guessedNum === ranNum) console.log("Parabéns, número correto!");
-  if (guessedNum !== ranNum) console.log(`Opa, não foi dessa vez. O número era ${ranNum}`);
+  if (guessedNum !== ranNum) console.log(`Ihhhhh errrou, não foi dessa vez. O número era ${ranNum}`);
 
   const playAgain = readline.question("Quer tentar novamente(s/n)? ");
 
